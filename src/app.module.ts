de3b5e56dd81712b30aa598a749378fab2ec9c08
jpena <juan.pena@wmcontractor.com>
2025-02-07 16:45:18 -0500

    Database postgresql set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,31 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { ItemsModule } from './items/items.module';
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from './config/envs';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false })],
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: envs.dbHost,
+      port: envs.dbPort,
+      username: envs.dbUsername,
+      password: envs.dbPassword,
+      database: envs.dbName,
+      entities: [],
+      synchronize: true,
     }),
     ItemsModule,
   ],
---
+++ src/config/envs.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface envVars {
+  DB_HOST: string;
+  DB_NAME: string;
+  DB_PASSWORD: string;
+  DB_PORT: string;
+  DB_USERNAME: string;
+}
+
+const envsSchema = joi
+  .object({
+    DB_USERNAME: joi.string().required(),
+    DB_HOST: joi.string().required(),
+    DB_PASSWORD: joi.string().required(),
+    DB_NAME: joi.string().required(),
+    DB_PORT: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value: envVars } = envsSchema.validate({
+  ...process.env,
+});
+
+if (error) {
+  throw new Error(`Config validation error ${error.message}`);
+}
+
+export const envs = {
+  dbHost: envVars.DB_HOST,
+  dbUsername: envVars.DB_USERNAME,
+  dbPassword: envVars.DB_PASSWORD,
+  dbName: envVars.DB_NAME,
+  dbPort: envVars.DB_PORT,
+};
