01e58f85b3724906c00ca450cdc91db2eeaa959c
jpena <juan.pena@wmcontractor.com>
2025-02-14 21:46:26 -0500

    Add roles validation

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,20 +1,34 @@
 import {
   ExecutionContext,
+  ForbiddenException,
   InternalServerErrorException,
   createParamDecorator,
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { ValidRoles } from '../enums/valid-roles.enum';
+import { User } from 'src/users/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
-  (roles = [], context: ExecutionContext) => {
+  (roles: ValidRoles[], context: ExecutionContext) => {
+    console.log(roles);
+
     const ctx = GqlExecutionContext.create(context);
-    const user = ctx.getContext().req.user;
+    const user: User = ctx.getContext().req.user;
 
     if (!user) {
       throw new InternalServerErrorException(
         `No user inside the request - make sure AuthGuard is used`,
       );
     }
-    return user;
+
+    if (roles.length === 0) return user;
+
+    for (const role of user.roles) {
+      if (roles.includes(role as ValidRoles)) {
+        return user;
+      }
+    }
+
+    throw new ForbiddenException(`User ${user.fullName} needs a valid role`);
   },
 );
